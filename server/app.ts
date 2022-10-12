7b212f1120545dfe63e96e239162efde760678da
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-12 13:28:17 +0300

    Create Project Structure

---
+++ server/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import { join } from 'path';
+require('env2')('.env');
+
+const app = express();
+const { NODE_ENV } = process.env;
+
+app.use([
+  compression(),
+  cookieParser(),
+  express.urlencoded({ extended: false }),
+]);
+
+app.set('port', process.env.PORT || 8080);
+
+app.get('/data', (req: Request, res:Response)=> res.send('Hello There!'));
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+export default app;

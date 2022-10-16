a2e1304b56f270ab7ff014a12eb03be6a9730ba8
Ibtisam Hemmo <ibtisamhemmo@gmail.com>
2022-10-16 21:43:42 +0300

    Update auth.ts

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,15 +1,16 @@
-import { Response, Request, NextFunction } from 'express';
-import CustomError from '../middlewares';
-import CustomRequest from '../middlewares/interfaces'
+import { Response, NextFunction } from 'express';
+import request from './interfaces/UserRequest';
+import { CustomError, verifyToken } from '../utils';
 
-const pharmacyAuth = async (req: CustomRequest, res: Response, next: NextFunction) => {
-  const role = req.user?.role;
-
-  if(role === 'pharmacy') {
-    next()
-  } else {
-    throw new CustomError(401, 'You are not authenticated');
+const auth = async (req: request, res: Response, next: NextFunction) => {
+  const { token } = req.cookies;
+  if (!token) throw new CustomError(401, 'Unauthorized');
+  try {
+    const user = await verifyToken(token);
+    req.user = user;
+  } catch (err) {
+    next(err);
   }
 };
 
-export default pharmacyAuth;
+export default auth;

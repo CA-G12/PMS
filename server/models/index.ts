e430fdcd753852a76d70669d1dd5211405ea223c
Nader-SH <nadershak149@gmail.com>
2022-10-16 10:04:39 +0300

    fix data relations

--- server/models/index.ts
+++ server/models/index.ts
@@ -6,18 +6,25 @@ import SalesHistory from './salesHistory';
 import ProductPharmacy from './productPharmacy';
 
 Pharmacy.hasMany(ProductPharmacy, { foreignKey: 'pharmacy_id' });
+ProductPharmacy.belongsTo(Pharmacy);
 
 Product.hasMany(ProductPharmacy, { foreignKey: 'product_id' });
+ProductPharmacy.belongsTo(Product);
 
 Pharmacy.hasMany(SalesHistory, { foreignKey: 'pharmacy_id' });
+SalesHistory.belongsTo(Pharmacy);
 
 Pharmacy.hasMany(ProductsRequest, { foreignKey: 'pharmacy_id' });
+ProductsRequest.belongsTo(Pharmacy);
 
 Product.hasMany(SalesHistory, { foreignKey: 'product_id' });
+SalesHistory.belongsTo(Product);
 
 Product.hasOne(ProductsRequest, { foreignKey: 'product_id' });
+ProductsRequest.belongsTo(Product);
 
 Product.hasMany(AdminProduct, { foreignKey: 'product_id' });
+AdminProduct.belongsTo(Product);
 
 export {
   AdminProduct,

45d3f5495fb39441353efd9a6d35cfe4bc1616a7
Nader-SH <nadershak149@gmail.com>
2022-10-16 16:14:41 +0300

    remove status code

--- server/controllers/authentication/auth.ts
+++ server/controllers/authentication/auth.ts
@@ -1,9 +1,8 @@
-// eslint-disable-next-line import/no-import-module-exports
 import { Response } from 'express';
 import { UserRequest } from '../../middlewares/interfaces';
 
 const checkAuthData = (req:UserRequest, res:Response) => {
-  res.status(200).json(req.user);
+  res.json(req.user);
 };
 
 export default checkAuthData;

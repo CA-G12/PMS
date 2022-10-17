77f5ab598c90e4f696e71226a1c799221957f279
Nader-SH <nadershak149@gmail.com>
2022-10-17 08:35:55 +0300

    delete the communts

--- server/controllers/authentication/index.ts
+++ server/controllers/authentication/index.ts
@@ -2,7 +2,6 @@ import checkAuthData from './auth';
 import logout from './logout';
 
 export {
-  // eslint-disable-next-line import/prefer-default-export
   checkAuthData,
   logout,
 };
--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,7 +1,6 @@
 import { checkAuthData, logout } from './authentication';
 
 export {
-  // eslint-disable-next-line import/prefer-default-export
   checkAuthData,
   logout,
 };

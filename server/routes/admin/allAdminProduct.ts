82400b1e84d807be095787073750ba2d22d57ecc
Nader shak-shak <64221231+Nader-SH@users.noreply.github.com>
2022-10-18 12:33:45 +0300

    Delete allAdminProduct.ts

--- server/routes/admin/allAdminProduct.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllProductsAdmin } from '../../controllers';
-
-const router: any = express.Router();
-
-router.get('/admin/products', getAllProductsAdmin);
-
-export default router;

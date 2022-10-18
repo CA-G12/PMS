523c735d67df302290e0b5e0a7aa793ea059fbd4
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-18 22:02:59 +0300

    Pharmacy Auth middleware

---
+++ server/middlewares/pharmacyAuth.ts
@@ -0,0 +1,15 @@
+import { Response, NextFunction } from 'express';
+import { CustomError } from '.';
+import { UserRequest } from './interfaces';
+
+const pharmacyAuth = async (req: UserRequest, res: Response, next: NextFunction) => {
+  const role = req.user?.role;
+
+  if (role === 'pharmacy') {
+    next();
+  } else {
+    next(new CustomError(401, 'You are not authenticated'));
+  }
+};
+
+export default pharmacyAuth;

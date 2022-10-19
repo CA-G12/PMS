4aaf67ee1db48fd1a90c146aa42afc999c61387f
Nader-SH <nadershak149@gmail.com>
2022-10-19 13:09:39 +0300

    add tests for get admin products

--- server/test/admin/requestStatus.test.ts
+++ server/test/admin/requestStatus.test.ts
@@ -1,8 +1,10 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-undef */
 import supertest from 'supertest';
+import { request } from 'http';
 import buildSeeds from '../../database/seeds/buildSeeds';
 import app from '../../app';
+import { getProductsAdmin, getOneProduct } from '../../queries';
 
 beforeAll(() => buildSeeds());
 
@@ -52,6 +54,41 @@ describe('Testing pages routers and its status codes', () => {
         expect(res.statusCode).toBe(400);
         done();
       });
+  });
+});
+
+describe('Testing pages routers for all admin products ', () => {
+  test('get all product admin  for admin', (done) => {
+    supertest(app)
+      .get('/api/v1/admin/products')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+  test('test get all products query', () => {
+    getProductsAdmin().then((data) => {
+      expect(data.length).toBeGreaterThan(0);
+    });
+  });
+});
+describe('Testing pages routers for all admin products by id ', () => {
+  test('get all product admin  for admin', (done) => {
+    supertest(app)
+      .get('/api/v1/admin/products/3')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+  test('test get all products query id ', () => {
+    getOneProduct(1).then((data) => {
+      expect(data.length).toEqual(1);
+    });
   });
 });
 

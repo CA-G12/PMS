eab51e8e6076f69732442fc2b786e88fbed289bc
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-20 19:47:42 +0300

    Edit router

--- server/queries/admin/overview.ts
+++ server/queries/admin/overview.ts
@@ -1,40 +1,42 @@
-import { Pharmacy, Product, ProductsRequest, AdminProduct } from "../../models";
+import {
+  Pharmacy, Product, ProductsRequest, AdminProduct,
+} from '../../models';
 
 const pharmacyCount = async () => Pharmacy.count();
 const pendingApplicationsCount = async () => Pharmacy.count({
-    where:{
-        status:'Pending'
-    }
+  where: {
+    status: 'Pending',
+  },
 });
 const closedApplicationsCount = async () => Pharmacy.count({
-    where:{
-        status:'Closed'
-    }
+  where: {
+    status: 'Closed',
+  },
 });
 const openedApplicationsCount = async () => Pharmacy.count({
-    where:{
-        status:'Opened'
-    }
+  where: {
+    status: 'Opened',
+  },
 });
 
 const productCount = async () => Product.count();
 
 const expiredProductsCount = async () => AdminProduct.findAndCountAll({
-    attributes:['expired_quantity']
+  attributes: ['expired_quantity'],
 });
 const inStockProductsCount = async () => AdminProduct.findAndCountAll({
-    attributes:['in_stock_quantity']
+  attributes: ['in_stock_quantity'],
 });
 
 const requestsCount = async () => ProductsRequest.count();
 
 export {
-    pharmacyCount,
-    productCount,
-    requestsCount,
-    pendingApplicationsCount,
-    openedApplicationsCount,
-    closedApplicationsCount,
-    expiredProductsCount,
-    inStockProductsCount
-}
+  pharmacyCount,
+  productCount,
+  requestsCount,
+  pendingApplicationsCount,
+  openedApplicationsCount,
+  closedApplicationsCount,
+  expiredProductsCount,
+  inStockProductsCount,
+};
--- server/test/admin/overview.test.ts
+++ server/test/admin/overview.test.ts
@@ -5,39 +5,26 @@ import app from '../../app';
 beforeAll(() => buildSeeds());
 
 describe('Admin Overview Statistics', () => {
-    test('Get Admin overview', (done) => {
-        supertest(app)
-          .get('/admin/statistics')
-          .expect('Content-Type', /json/)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.statusCode).toBe(200);
-            return done();
-          });
+  test('Get Admin overview', (done) => {
+    supertest(app)
+      .get('/admin/statistics')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(200);
+        return done();
       });
+  });
 
-    test('Get Admin overview and the data received', (done) => {
-        supertest(app)
-          .get('/admin/statistics')
-          .expect('Content-Type', /json/)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.statusCode).toBe(200);
-            expect(res.body.msg).toBe('Statistics are sent successfully');
-            return done();
-          });
+  test('Get Admin overview and the data received', (done) => {
+    supertest(app)
+      .get('/admin/statistics')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.msg).toBe('Statistics are sent successfully');
+        return done();
       });
-
-      test('Get Admin overview and the data received', (done) => {
-        supertest(app)
-          .get('/admin/statistics')
-          .expect('Content-Type', /json/)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.statusCode).toBe(200);
-            expect(res.body.msg).toBe('Statistics are sent successfully');
-            return done();
-          });
-      });
-
-})
+  });
+});

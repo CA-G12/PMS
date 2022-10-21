1363efcffe8fcc5c98ea1dbcbeb20a9cac966f43
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-21 22:16:10 +0300

    Edit router for overview

--- server/routes/admin/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAdminOverview } from "../../controllers";
-import adminAuth from "../../middlewares/adminAuth";
-
-const adminRouter = Router();
-
-adminRouter.get('/admin/statistics', adminAuth, getAdminOverview);
-
-export default adminRouter;

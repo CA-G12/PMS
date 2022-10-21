84a455762f2eff443653c1742dbbed157df5f690
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-21 22:11:36 +0300

    Pass auth middleware

--- server/routes/admin/index.ts
+++ server/routes/admin/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { requestStatus, getAllPharmacies } from '../../controllers';
+import { auth } from '../../middlewares';
 import adminAuth from '../../middlewares/adminAuth';
 
 const adminRouter = Router();
 
-adminRouter.get('/admin/pharmacies', adminAuth, getAllPharmacies);
-adminRouter.put('/admin/requests/:requestId', adminAuth, requestStatus);
+adminRouter.get('/admin/pharmacies', auth, adminAuth, getAllPharmacies);
+adminRouter.put('/admin/requests/:requestId', auth, adminAuth, requestStatus);
 
 export default adminRouter;

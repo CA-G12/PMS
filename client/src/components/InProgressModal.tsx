a6e548e32c010205594163888e76af61639fdb7d
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-21 23:13:18 +0300

    Separate style to an external file

--- client/src/components/InProgressModal.tsx
+++ client/src/components/InProgressModal.tsx
@@ -1,28 +1,12 @@
 import * as React from 'react';
 import { Box, Button, Typography, Modal } from '@mui/material';
 import 'typeface-mulish';
+import './style.css';
 
 const InProgress = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
-  const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '25vw',
-    height: '50vh',
-    bgcolor: 'background.paper',
-    borderRadius: '3px',
-    boxShadow: 24,
-    p: 4,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
 
   return (
     <>
@@ -31,11 +15,11 @@ const InProgress = () => {
         open={open}
         onClose={handleClose}
       >
-        <Box sx={style}>
+        <Box className='boxModal'>
           <img
             alt="in progress"
             src="https://cdn.dribbble.com/users/508543/screenshots/7122590/media/7b5c4f53467d3f586a06ec39f821b084.gif"
-            style={{ height: '90%' }}
+            style={{ height: '80%' }}
           />
           <Typography
             variant="h6"

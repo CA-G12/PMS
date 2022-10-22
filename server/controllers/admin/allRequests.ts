02efad3152202aa040af58fea011750672c2062c
Ahmed Abu Sharar <absam9945@gmail.com>
2022-10-22 16:21:02 +0300

    delete rule

--- server/controllers/admin/allRequests.ts
+++ server/controllers/admin/allRequests.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
 import { requestQuery } from '../../queries';
-import { CustomError } from '../../utils';
 
 const allRequests = async (req : Request, res : Response, next : NextFunction) => {
   const { numOffSet } = req.query;

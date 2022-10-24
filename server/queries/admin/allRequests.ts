5cf1e1468c2b899b52f1eb1a5519754319c531b7
Ahmed Abu Sharar <absam9945@gmail.com>
2022-10-24 14:20:53 +0300

    edit request query

--- server/queries/admin/allRequests.ts
+++ server/queries/admin/allRequests.ts
@@ -1,8 +1,21 @@
 import {
   ProductsRequest as productsRequestModel,
+  Pharmacy as PharmacyModel,
+  Product as ProductModel,
 } from '../../models';
 
 const requestQuery = (numOffSet: number) => productsRequestModel.findAndCountAll({
+  attributes: ['id', 'status'],
+  include: [
+    {
+      model: PharmacyModel,
+      attributes: ['name'],
+    },
+    {
+      model: ProductModel,
+      attributes: ['name'],
+    },
+  ],
   offset: 7 * (numOffSet - 1),
   limit: 7,
 });

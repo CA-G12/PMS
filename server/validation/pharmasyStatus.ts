9c79aebf2d98b8ee28e99c1a14271b749cf5bd86
Nader-SH <nadershak149@gmail.com>
2022-10-24 13:16:34 +0300

    update the validation to Opened, Closed, Rejected, Pending

--- server/validation/pharmasyStatus.ts
+++ server/validation/pharmasyStatus.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 const pharmacyStatusval = Joi.object({
-  status: Joi.string().valid('Pending', 'Rejected', 'Approved'),
+  status: Joi.string().valid('Opened', 'Closed', 'Rejected', 'Pending'),
   pharmacyId: Joi.number().required(),
 });
 

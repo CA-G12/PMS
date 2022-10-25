2c27baa081294d275d2f7c4b94d15d2c024f8d8f
Nader-SH <nadershak149@gmail.com>
2022-10-25 12:55:08 +0300

    change to try and catch

--- client/src/pages/AllAdminPharmasis/AdminAllPharmasis.tsx
+++ client/src/pages/AllAdminPharmasis/AdminAllPharmasis.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,7 +10,6 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
-import { useState } from 'react';
 import CustomizedInputBase from './Extra/Search';
 import LongMenu from './Extra/Options';
 import image31 from '../../assets/image31.png';
@@ -18,17 +18,58 @@ const options = ['Opened', 'Closed'];
 
 const AllAdminPharmasis = () => {
   const [data, setData] = useState([]);
-  const setStatus = async (status: string, PharmacyId: number) => {
-    await axios.put(`/admin/pharmacy/${PharmacyId}`, {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const updatePharmacyStatus = (status: string, pharmacyId: number) =>
+    axios.put(`/admin/pharmacy/${pharmacyId}`, {
       status,
     });
+
+  const getPharmacies = async () => {
+    const res = await axios.get('/admin/pharmacies');
+    return res.data.data.rows;
   };
-  axios({
-    method: 'get',
-    url: '/admin/pharmacies',
-  }).then((response) => {
-    setData(response.data.data.rows);
-  });
+
+  const setStatus = async (status: string, pharmacyId: number) => {
+    try {
+      setLoading(true);
+
+      await updatePharmacyStatus(status, pharmacyId);
+      const pharmacies = await getPharmacies();
+      setData(pharmacies);
+
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      // toast.error('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+
+        const pharmacies = await getPharmacies();
+
+        setData(pharmacies);
+        setLoading(false);
+      } catch (err) {
+        setError('Somethig went wrong.');
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  if (loading) {
+    return <Box>loading</Box>;
+  }
+
+  if (error) {
+    return <Box>{error}</Box>;
+  }
+
   return (
     <Box>
       <TableContainer component={Paper}>
@@ -59,7 +100,7 @@ const AllAdminPharmasis = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((row) => (
+            {data.map((row: any) => (
               <TableRow
                 key={row.id}
                 sx={{

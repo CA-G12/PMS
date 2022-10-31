3740cc8884971214463f82640d89f7d4fca8e69b
Ibtisam Hemmo <ibtisamhemmo@gmail.com>
2022-10-31 12:01:00 +0200

    Delete pharamcyRequests.ts

--- server/routes/pharamcies/pharamcyRequests.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import getPharmacyRequests from '../../controllers/pharamcies';
-
-const pharmacyRoutes = Router();
-
-pharmacyRoutes.get('/pharmacy/pharmacyId/requests', getPharmacyRequests);
-
-export default pharmacyRoutes;

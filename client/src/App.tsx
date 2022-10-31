f63c5c773c7a6eab2ad831a1d41b98849089fcfe
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-31 22:49:55 +0200

    Add Loading Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,50 +11,48 @@ import ApplicationSection from './components/admin/ApplicationSection';
 import { AuthProvider } from './context/authContext';
 import Login from './components/auth/login';
 import Signup from './components/auth/Signup';
-import GeneralInfo from './components/pharmacy/GeneralInfo';
-const App = () => <GeneralInfo />;
 
-// const App = () => {
-//   const router = createBrowserRouter([
-//     {
-//       path: '/admin',
-//       element: <DashboardLayout />,
-//       children: [
-//         {
-//           path: 'overview',
-//           element: <Overview />,
-//         },
-//         {
-//           path: 'pharmacies',
-//           element: <Pharmacies />,
-//         },
-//         {
-//           path: 'products',
-//           element: <Products />,
-//         },
-//         {
-//           path: 'requests',
-//           element: <Requests />,
-//         },
-//         {
-//           path: 'applications',
-//           element: <ApplicationSection />,
-//         },
-//       ],
-//     },
-//     {
-//       path: '/login',
-//       element: <Login />,
-//     },
-//     {
-//       path: '/signUp',
-//       element: <Signup />,
-//     },
-//   ]);
-//   return (
-//     <AuthProvider>
-//       <RouterProvider router={router} />
-//     </AuthProvider>
-//   );
-// };
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/admin',
+      element: <DashboardLayout />,
+      children: [
+        {
+          path: 'overview',
+          element: <Overview />,
+        },
+        {
+          path: 'pharmacies',
+          element: <Pharmacies />,
+        },
+        {
+          path: 'products',
+          element: <Products />,
+        },
+        {
+          path: 'requests',
+          element: <Requests />,
+        },
+        {
+          path: 'applications',
+          element: <ApplicationSection />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/signUp',
+      element: <Signup />,
+    },
+  ]);
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+};
 export default App;

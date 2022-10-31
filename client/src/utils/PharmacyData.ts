9e4a4d84c219f63f065aba407e938ddb24bf2165
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-31 22:30:41 +0200

    Edit hook to get data

---
+++ client/src/utils/PharmacyData.ts
@@ -0,0 +1,11 @@
+interface pharmacyData  {
+    name: string,
+    location:string,
+    email:string,
+    phone:string,
+    description:string,
+    owner_name:string,
+    owner_img:string,
+  }
+
+  export default pharmacyData;

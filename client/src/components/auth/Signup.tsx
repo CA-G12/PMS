63e01cade38102f7ad8c374b7d2146e591c4d36d
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2022-10-31 21:08:58 +0200

    Add Navbar to Both Login and Sign up pages

--- client/src/components/auth/Signup.tsx
+++ client/src/components/auth/Signup.tsx
@@ -5,6 +5,7 @@ import { Box, FormLabel, Input, Typography } from '@mui/material';
 import 'typeface-mulish';
 import ButtonComponent from '../Button';
 import InputForm from '../InputForm';
+import Navbar from '../NavBar/Navbar';
 
 const Signup: React.FC = () => {
   const [fullName, setFullName] = useState('');
@@ -62,162 +63,166 @@ const Signup: React.FC = () => {
   };
 
   return (
-    <Box
-      sx={{
-        width: '50vw',
-        backgroundColor: '#fdfdfd',
-        boxShadow: '0px 0px 14px #0000001c',
-        margin: '0 auto',
-        marginBottom: '50px',
-      }}
-    >
-      <Typography
-        variant="h5"
-        fontFamily="mulish"
-        fontWeight={800}
-        margin="40px"
-        pt="20px"
-        display="flex"
-        justifyContent="center"
-        sx={{ color: '#617BAD', textShadow: '6px 3px 14px #6D85B3' }}
-      >
-        Sign up Your Pharmacy
-      </Typography>
-      <Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            marginBottom: '20px',
-          }}
-        >
-          <InputForm
-            label="Full Name"
-            state={fullName}
-            setState={setFullName}
-          />
-          <InputForm label="OwnerID" state={ownerID} setState={setOwnerID} />
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            marginBottom: '20px',
-          }}
-        >
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <InputForm
-              label="Pharmacy Name"
-              state={pharmacyName}
-              setState={setPharmacyName}
-            />
-          </Box>
-
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <InputForm
-              label="License Number"
-              state={licenseNumber}
-              setState={setLicenseNumber}
-            />
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            marginBottom: '20px',
-          }}
-        >
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <InputForm
-              label="Pharmacy Location"
-              state={pharmacyLocation}
-              setState={setPharmacyLocation}
-            />
-          </Box>
-
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <InputForm
-              label="Phone Number"
-              state={phoneNumber}
-              setState={setPhoneNumber}
-            />
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            marginBottom: '20px',
-            marginLeft: '37px',
-          }}
-        >
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <FormLabel
-              sx={{
-                fontFamily: 'Mulish',
-                fontWeight: 'bold',
-                color: '#000000a6',
-              }}
-            >
-              Email
-            </FormLabel>
-            <Input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              sx={{
-                padding: '4px 0 5px',
-                border: '1px solid #00000026',
-                height: '45px',
-                width: '94%',
-                marginTop: '10px  ',
-              }}
-            />
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            marginBottom: '5px',
-          }}
-        >
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <InputForm
-              label="Password"
-              state={password}
-              setState={setPassword}
-            />
-          </Box>
-
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <InputForm
-              label="Confirm Password"
-              state={confirmPassword}
-              setState={setConfirmPassword}
-            />
-          </Box>
-        </Box>
-        <Typography
-          variant="caption"
-          display="block"
-          fontFamily="mulish"
-          marginLeft="35px"
-        >
-          You already have an account?{' '}
-          <span style={{ color: '#83B239' }}>Login</span>
-        </Typography>
-      </Box>
+    <>
+      <Navbar />
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          marginRight: '20px',
+          width: '50vw',
+          backgroundColor: '#fdfdfd',
+          boxShadow: '0px 0px 14px #0000001c',
+          margin: '0 auto',
+          marginBottom: '50px',
+          marginTop: '75px',
         }}
       >
-        <ButtonComponent text="Sign up" sendUserData={sendUserData} />
+        <Typography
+          variant="h5"
+          fontFamily="mulish"
+          fontWeight={800}
+          margin="40px"
+          pt="20px"
+          display="flex"
+          justifyContent="center"
+          sx={{ color: '#617BAD', textShadow: '6px 3px 14px #6D85B3' }}
+        >
+          Sign up Your Pharmacy
+        </Typography>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              marginBottom: '20px',
+            }}
+          >
+            <InputForm
+              label="Full Name"
+              state={fullName}
+              setState={setFullName}
+            />
+            <InputForm label="OwnerID" state={ownerID} setState={setOwnerID} />
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              marginBottom: '20px',
+            }}
+          >
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <InputForm
+                label="Pharmacy Name"
+                state={pharmacyName}
+                setState={setPharmacyName}
+              />
+            </Box>
+
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <InputForm
+                label="License Number"
+                state={licenseNumber}
+                setState={setLicenseNumber}
+              />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              marginBottom: '20px',
+            }}
+          >
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <InputForm
+                label="Pharmacy Location"
+                state={pharmacyLocation}
+                setState={setPharmacyLocation}
+              />
+            </Box>
+
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <InputForm
+                label="Phone Number"
+                state={phoneNumber}
+                setState={setPhoneNumber}
+              />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              marginBottom: '20px',
+              marginLeft: '37px',
+            }}
+          >
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <FormLabel
+                sx={{
+                  fontFamily: 'Mulish',
+                  fontWeight: 'bold',
+                  color: '#000000a6',
+                }}
+              >
+                Email
+              </FormLabel>
+              <Input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                sx={{
+                  padding: '4px 0 5px',
+                  border: '1px solid #00000026',
+                  height: '45px',
+                  width: '94%',
+                  marginTop: '10px  ',
+                }}
+              />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              marginBottom: '5px',
+            }}
+          >
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <InputForm
+                label="Password"
+                state={password}
+                setState={setPassword}
+              />
+            </Box>
+
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <InputForm
+                label="Confirm Password"
+                state={confirmPassword}
+                setState={setConfirmPassword}
+              />
+            </Box>
+          </Box>
+          <Typography
+            variant="caption"
+            display="block"
+            fontFamily="mulish"
+            marginLeft="35px"
+          >
+            You already have an account?{' '}
+            <span style={{ color: '#83B239' }}>Login</span>
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            marginRight: '20px',
+          }}
+        >
+          <ButtonComponent text="Sign up" sendUserData={sendUserData} />
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
